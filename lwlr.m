bd4ad9ae0ff5b40a3f2134df3693f1bfc21a9eb8
solA_aloha <sora410671760@outlook.com>
2017-09-12 14:39:17 +0800

    Delete lwlr.m

--- lwlr.m
+++
@@ -1,9 +0,0 @@
-% 对所有点计算估计值
-% cite: http://lib.csdn.net/article/machinelearning/35027
-function yHat = lwlr(testMat, xMat, yMat, k)
-    [row, ~] = size(testMat);
-    yHat = zeros(1, row);
-    for i = 1:1:row
-        yHat(i) = lwlrPoint(testMat(i,:), xMat, yMat, k);
-    end
-end
